0c1c1c31201fe2beb513e8519d6d19c23e71fa8b
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-05-31 17:29:14 +0530

    dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,23 +1,52 @@
 import cocotb
-from cocotb.triggers import RisingEdge
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
 
 @cocotb.test()
-async def basic_test(dut):
-    """Simple test for reset and I/O."""
+async def test_fifo_write_read(dut):
+    """ Test basic write to FIFO and read from output """
 
-    dut.rst_n.value = 0
-    for _ in range(2):
-        await RisingEdge(dut.clk)
-    dut.rst_n.value = 1
+    dut.RST_N.value = 0
+    dut.CLK.value = 0
+    dut.write_en.value = 0
+    dut.write_address.value = 0
+    dut.write_data.value = 0
+    dut.read_en.value = 0
+    dut.read_address.value = 0
 
+ 
+    clock = Clock(dut.CLK, 10, units="ns")  
+    cocotb.start_soon(clock.start())
+
+
+    await Timer(20, units='ns')
+    dut.RST_N.value = 1
+    await RisingEdge(dut.CLK)
+
+    dut.write_address.value = 4
+    dut.write_data.value = 1
     dut.write_en.value = 1
-    dut.data_in.value = 0xA5
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.CLK)
     dut.write_en.value = 0
 
-    dut.read_en.value = 1
-    await RisingEdge(dut.clk)
-    dut.read_en.value = 0
+    await RisingEdge(dut.CLK)
+    dut.write_address.value = 5
+    dut.write_data.value = 0
+    dut.write_en.value = 1
+    await RisingEdge(dut.CLK)
+    dut.write_en.value = 0
 
-    await RisingEdge(dut.clk)
-    cocotb.log.info(f"Read value: {dut.data_out.value}")
+    for _ in range(50):
+        await RisingEdge(dut.CLK)
+
+    dut.read_address.value = 3
+    dut.read_en.value = 1
+    await RisingEdge(dut.CLK)
+    dut.read_en.value = 0
+    await RisingEdge(dut.CLK)
+
+    # Read data
+    read_data = dut.read_data.value.integer
+    cocotb.log.info(f"Read data from y FIFO first element: {read_data}")
+
+    assert read_data == 1, f"Expected 1, got {read_data}"
