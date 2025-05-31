df193cf5f0f543491be5c77c335630143212a36d
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-05-31 16:24:56 +0530

    FIFO2.v

--- hdl/FIFO2.v
+++ hdl/FIFO2.v
@@ -1,8 +1,3 @@
-`ifdef BSV_ASSIGNMENT_DELAY
-`else
-  `define BSV_ASSIGNMENT_DELAY
-`endif
-
 `ifdef BSV_POSITIVE_RESET
   `define BSV_RESET_VALUE 1'b1
   `define BSV_RESET_EDGE posedge
@@ -11,142 +6,94 @@
   `define BSV_RESET_EDGE negedge
 `endif
 
-`ifdef BSV_ASYNC_RESET
- `define BSV_ARESET_EDGE_META or `BSV_RESET_EDGE RST
-`else
- `define BSV_ARESET_EDGE_META
-`endif
+module FIFO2 #(parameter width = 1) (
+    input                 CLK,
+    input                 RST,
+    input  [width-1:0]    D_IN,
+    input                 ENQ,
+    input                 DEQ,
+    input                 CLR,
 
-`ifdef BSV_RESET_FIFO_HEAD
- `define BSV_ARESET_EDGE_HEAD `BSV_ARESET_EDGE_META
-`else
- `define BSV_ARESET_EDGE_HEAD
-`endif
+    output                FULL_N,
+    output                EMPTY_N,
+    output [width-1:0]    D_OUT
+);
 
-// Depth 2 FIFO
-module FIFO2(CLK,
-             RST,
-             D_IN,
-             ENQ,
-             FULL_N,
-             D_OUT,
-             DEQ,
-             EMPTY_N,
-             CLR);
+  // Internal registers
+  reg [1:0] count;               // number of elements in FIFO: 0,1,2
+  reg [width-1:0] data0_reg;
+  reg [width-1:0] data1_reg;
 
-   parameter width = 1;
-   parameter guarded = 1'b1;
+  // Outputs
+  assign FULL_N  = (count != 2); // FULL_N = 1 means NOT full
+  assign EMPTY_N = (count != 0); // EMPTY_N = 1 means NOT empty
+  assign D_OUT   = data0_reg;
 
-   input     CLK ;
-   input     RST ;
-   input [width - 1 : 0] D_IN;
-   input                 ENQ;
-   input                 DEQ;
-   input                 CLR ;
-
-   output                FULL_N;
-   output                EMPTY_N;
-   output [width - 1 : 0] D_OUT;
-
-   reg                    full_reg;
-   reg                    empty_reg;
-   reg [width - 1 : 0]    data0_reg;
-   reg [width - 1 : 0]    data1_reg;
-
-   assign                 FULL_N = full_reg ;
-   assign                 EMPTY_N = empty_reg ;
-   assign                 D_OUT = data0_reg ;
-
-
-   // Optimize the loading logic since state encoding is not power of 2!
-   wire                   d0di = (ENQ && ! empty_reg ) || ( ENQ && DEQ && full_reg ) ;
-   wire                   d0d1 = DEQ && ! full_reg ;
-   wire                   d0h = ((! DEQ) && (! ENQ )) || (!DEQ && empty_reg ) || ( ! ENQ &&full_reg) ;
-   wire                   d1di = ENQ & empty_reg ;
-
-`ifdef BSV_NO_INITIAL_BLOCKS
-`else // not BSV_NO_INITIAL_BLOCKS
-   // synopsys translate_off
-   initial
-     begin
-        data0_reg   = {((width + 1)/2) {2'b10}} ;
-        data1_reg  = {((width + 1)/2) {2'b10}} ;
-        empty_reg = 1'b0;
-        full_reg  = 1'b1;
-     end // initial begin
-   // synopsys translate_on
-`endif // BSV_NO_INITIAL_BLOCKS
-
-   always@(posedge CLK `BSV_ARESET_EDGE_META)
-     begin
-        if (RST == `BSV_RESET_VALUE)
-          begin
-             empty_reg <= `BSV_ASSIGNMENT_DELAY 1'b0;
-             full_reg  <= `BSV_ASSIGNMENT_DELAY 1'b1;
-          end // if (RST == `BSV_RESET_VALUE)
-        else
-          begin
-             if (CLR)
-               begin
-                  empty_reg <= `BSV_ASSIGNMENT_DELAY 1'b0;
-                  full_reg  <= `BSV_ASSIGNMENT_DELAY 1'b1;
-               end // if (CLR)
-             else if ( ENQ && ! DEQ ) // just enq
-               begin
-                  empty_reg <= `BSV_ASSIGNMENT_DELAY 1'b1;
-                  full_reg <= `BSV_ASSIGNMENT_DELAY ! empty_reg ;
-               end
-             else if ( DEQ && ! ENQ )
-               begin
-                  full_reg  <= `BSV_ASSIGNMENT_DELAY 1'b1;
-                  empty_reg <= `BSV_ASSIGNMENT_DELAY ! full_reg;
-               end // if ( DEQ && ! ENQ )
-          end // else: !if(RST == `BSV_RESET_VALUE)
-
-     end // always@ (posedge CLK or `BSV_RESET_EDGE RST)
-
-
-   always@(posedge CLK `BSV_ARESET_EDGE_HEAD)
-     begin
-`ifdef BSV_RESET_FIFO_HEAD
-        if (RST == `BSV_RESET_VALUE)
-          begin
-             data0_reg <= `BSV_ASSIGNMENT_DELAY {width {1'b0}} ;
-             data1_reg <= `BSV_ASSIGNMENT_DELAY {width {1'b0}} ;
+  // Sequential logic for count and data registers
+  always @(posedge CLK `BSV_RESET_EDGE) begin
+    if (RST == `BSV_RESET_VALUE || CLR) begin
+      count    <= 0;
+      data0_reg <= {width{1'b0}};
+      data1_reg <= {width{1'b0}};
+    end else begin
+      // Enqueue and Dequeue combinations
+      case ({ENQ, DEQ})
+        2'b10: begin // Enqueue only
+          if (count < 2) begin
+            if (count == 0) begin
+              data0_reg <= D_IN;
+            end else if (count == 1) begin
+              data1_reg <= D_IN;
+            end
+            count <= count + 1;
           end
-        else
-`endif
-          begin
-             data0_reg  <= `BSV_ASSIGNMENT_DELAY
-                           {width{d0di}} & D_IN | {width{d0d1}} & data1_reg | {width{d0h}} & data0_reg ;
-             data1_reg <= `BSV_ASSIGNMENT_DELAY
-                          d1di ? D_IN : data1_reg ;
-          end // else: !if(RST == `BSV_RESET_VALUE)
-     end // always@ (posedge CLK or `BSV_RESET_EDGE RST)
+        end
 
-
-
-   // synopsys translate_off
-   always@(posedge CLK)
-     begin: error_checks
-        reg deqerror, enqerror ;
-
-        deqerror =  0;
-        enqerror = 0;
-        if (RST == ! `BSV_RESET_VALUE)
-          begin
-             if ( ! empty_reg && DEQ )
-               begin
-                  deqerror =  1;
-                  $display( "Warning: FIFO2: %m -- Dequeuing from empty fifo" ) ;
-               end
-             if ( ! full_reg && ENQ && (!DEQ || guarded) )
-               begin
-                  enqerror = 1;
-                  $display( "Warning: FIFO2: %m -- Enqueuing to a full fifo" ) ;
-               end
+        2'b01: begin // Dequeue only
+          if (count > 0) begin
+            data0_reg <= data1_reg; // shift data1 to data0
+            data1_reg <= {width{1'b0}};
+            count <= count - 1;
           end
-     end // always@ (posedge CLK)
-   // synopsys translate_on
+        end
+
+        2'b11: begin // Enqueue and Dequeue simultaneously
+          if (count == 0) begin
+            // FIFO empty, enqueue data0
+            data0_reg <= D_IN;
+            count <= 1;
+          end else if (count == 1) begin
+            // shift data1 to data0, enqueue new data into data1
+            data0_reg <= data1_reg;
+            data1_reg <= D_IN;
+            // count remains 1
+          end else if (count == 2) begin
+            // FIFO full, dequeue oldest (data0), shift data1 to data0, enqueue new data to data1
+            data0_reg <= data1_reg;
+            data1_reg <= D_IN;
+            // count remains 2
+          end
+        end
+
+        default: begin
+          // No enqueue or dequeue, hold state
+        end
+      endcase
+    end
+  end
+
+  // Error checking (simulation only)
+  // synopsys translate_off
+  always @(posedge CLK) begin
+    if (RST != `BSV_RESET_VALUE) begin
+      if (ENQ && (count == 2)) begin
+        $display("Warning: FIFO2: %m -- Enqueuing to full FIFO");
+      end
+      if (DEQ && (count == 0)) begin
+        $display("Warning: FIFO2: %m -- Dequeuing from empty FIFO");
+      end
+    end
+  end
+  // synopsys translate_on
 
 endmodule
