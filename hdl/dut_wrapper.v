c9012bbbaa58c6050785dc361208a8a736d8c15b
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-06-03 15:15:40 +0530

    dut_wrapper.v

--- hdl/dut_wrapper.v
+++ hdl/dut_wrapper.v
@@ -1,27 +1,27 @@
-module dut_wrapper(
-    input  wire        clk,
-    input  wire        reset_n,
-    input  wire [2:0]  write_address,
-    input  wire [7:0]  write_data, 
-    input  wire        write_en,
-    output wire        write_rdy,
-    input  wire [2:0]  read_address,
-    input  wire        read_en,
-    output wire [7:0]  read_data,  
-    output wire        read_rdy
+module dut_wrapper (
+    input clk,
+    input reset_n,
+    input write_en,
+    input [2:0] write_address,
+    input [7:0] write_data,
+    input read_en,
+    input [2:0] read_address,
+    output [7:0] read_data,
+    output [7:0] counter_out  
 );
-    dut dut_inst (
-        .CLK(clk),
-        .RST_N(reset_n),
-        .write_address(write_address),
-        .write_data(write_data), 
-        .write_en(write_en),
-        .write_rdy(write_rdy),
-        .read_address(read_address),
-        .read_en(read_en),
-        .read_data(read_data),   
-        .read_rdy(read_rdy)
-    );
+
+dut u_dut (
+    .clk(clk),
+    .reset_n(reset_n),
+    .write_en(write_en),
+    .write_address(write_address),
+    .write_data(write_data),
+    .read_en(read_en),
+    .read_address(read_address),
+    .read_data(read_data),
+    .counter_out(counter_out) 
+);
+
     
     initial begin
         $dumpfile("waveform.vcd");
