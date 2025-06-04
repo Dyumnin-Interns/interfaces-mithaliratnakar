1d0cf61bbf0b23dc4a6dc24e3ad1a2977f952486
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-06-04 07:04:23 +0530

    dut_wrapper.v

--- hdl/dut_wrapper.v
+++ hdl/dut_wrapper.v
@@ -1,16 +1,16 @@
 module dut_wrapper (
     input wire CLK,
     input wire RST,
-    input wire [0:0] DATA_IN,     
+    input wire [7:0] DATA_IN,     
     input wire WR_EN,
     input wire RD_EN,
     input wire CLR,
-    output wire [0:0] DATA_OUT,
+    output wire [7:0] DATA_OUT,
     output wire EMPTY,
     output wire FULL
 );
 
-    FIFO1 #(.width(1)) fifo_inst (
+    FIFO1 #(.width(8)) fifo_inst (
         .CLK(CLK),
         .RST(RST),
         .D_IN(DATA_IN),
