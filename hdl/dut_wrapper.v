795d9b0f6d220c4740e779748e0366232b9850c4
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-06-17 18:40:21 +0530

    dut_wrapper.v

--- hdl/dut_wrapper.v
+++ hdl/dut_wrapper.v
@@ -1,7 +1,7 @@
 module dut_wrapper (
     input wire CLK,
     input wire RST,
-    input wire [7:0] DATA_IN,     
+    input wire [7:0] DATA_IN,
     input wire WR_EN,
     input wire RD_EN,
     input wire CLR,
@@ -9,20 +9,24 @@ module dut_wrapper (
     output wire EMPTY,
     output wire FULL
 );
+    wire full_n, empty_n;
 
     FIFO1 #(.width(8)) fifo_inst (
         .CLK(CLK),
-        .RST(RST),
+        .RST(RST_N),
         .D_IN(DATA_IN),
         .ENQ(WR_EN),
         .DEQ(RD_EN),
         .CLR(CLR),
         .D_OUT(DATA_OUT),
-        .EMPTY_N(EMPTY),
-        .FULL_N(FULL)
+        .EMPTY_N(empty_n),
+        .FULL_N(full_n)
     );
 
-    initial begin
+    assign EMPTY = ~empty_n;
+    assign FULL  = ~full_n;
+
+initial begin
         $dumpfile("waveform.vcd");
         $dumpvars(0, dut_wrapper);
     end
