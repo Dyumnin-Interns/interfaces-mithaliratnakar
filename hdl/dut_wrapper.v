faed1f733831710dccac8fe9857b20a616a4bdab
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-06-23 01:32:02 +0530

    dut_wrapper.v

--- hdl/dut_wrapper.v
+++ hdl/dut_wrapper.v
@@ -1,34 +1,35 @@
 module dut_wrapper (
     input wire CLK,
-    input wire RST,
-    input wire [7:0] DATA_IN,
-    input wire WR_EN,
-    input wire RD_EN,
-    input wire CLR,
-    output wire [7:0] DATA_OUT,
-    output wire EMPTY,
-    output wire FULL
+    input wire RST_N,
+    input wire [2:0] write_address,
+    input wire write_data,
+    input wire write_en,
+    output wire write_rdy,
+    input wire [2:0] read_address,
+    input wire read_en,
+    output wire read_data,
+    output wire read_rdy,
+    output wire a_ff_dout,
+    output wire b_ff_dout,
+    output wire y_ff_dout
 );
-    wire full_n, empty_n;
-
-    FIFO1 #(.width(8)) fifo_inst (
-        .CLK(CLK),
-        .RST(RST_N),
-        .D_IN(DATA_IN),
-        .ENQ(WR_EN),
-        .DEQ(RD_EN),
-        .CLR(CLR),
-        .D_OUT(DATA_OUT),
-        .EMPTY_N(empty_n),
-        .FULL_N(full_n)
-    );
-
-    assign EMPTY = ~empty_n;
-    assign FULL  = ~full_n;
-
+  dut dut_inst (
+      .CLK(CLK),
+      .RST_N(RST_N),
+      .write_address(write_address),
+      .write_data(write_data),
+      .write_en(write_en),
+      .write_rdy(write_rdy),
+      .read_address(read_address),
+      .read_en(read_en),
+      .read_data(read_data),
+      .read_rdy(read_rdy)
+  );
+  assign a_ff_dout = dut_inst.a_ff$D_OUT;
+  assign b_ff_dout = dut_inst.b_ff$D_OUT;
+  assign y_ff_dout = dut_inst.y_ff$D_OUT;
 initial begin
-        $dumpfile("waveform.vcd");
-        $dumpvars(0, dut_wrapper);
-    end
-
+  $dumpfile("waveform.vcd");
+  $dumpvars(0, dut_wrapper);
+end
 endmodule
