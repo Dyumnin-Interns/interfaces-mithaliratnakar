e90ea87a681da6092cdb9652ae21917a9b6efcf5
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-06-23 15:10:56 +0530

    monitor.py

--- tests/monitor.py
+++ tests/monitor.py
@@ -1,18 +1,10 @@
-import cocotb
 from cocotb.triggers import RisingEdge
 
 class DutMonitor:
-    def __init__(self, dut, callback):
+    def __init__(self, dut, callback=None):
         self.dut = dut
         self.callback = callback
-        self._monitor_task = None
 
     def start(self):
-        self._monitor_task = cocotb.start_soon(self._monitor_output())
+        pass 
 
-    async def _monitor_output(self):
-        while True:
-            await RisingEdge(self.dut.CLK)
-            if self.dut.read_en.value == 1 and self.dut.read_rdy.value == 1:
-                read_val = int(self.dut.read_data.value)
-                self.callback(read_val)
